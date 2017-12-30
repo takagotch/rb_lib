085558e25ab8dc7f88587de4c8035f84edc41562
takagotch <dyaccb@gmail.com>
2017-12-30 15:12:18 +0900

    cgi

---
+++ cgi.rb
@@ -0,0 +1,37 @@
+require 'cgi'
+
+CGI.escape()
+CGI.unescape()
+CGI::escapeHTML()
+
+CGI.unescepeHTML()
+CGI.unescepeHTML()
+str = ''
+str.force_encoding()
+CGI.unescapeHTML(str)
+
+
+require 'cgi'
+c = CGI.new
+c.auth_type
+c.user_agent
+
+require 'cgi'
+c = CGI.new
+c[]
+c.keys
+c.params
+
+requrie 'cgi'
+c = CGI.new
+c[]
+c.params[]
+c.keys
+c.params
+
+require ''
+
+require ''
+
+require ''
+
